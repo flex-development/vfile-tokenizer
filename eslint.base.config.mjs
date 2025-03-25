22bfb07d816f71ddfdfcabba525c870faf22beb0
Lexus Drumgold <unicornware@flexdevelopment.llc>
2025-03-25 16:29:40 -0400

    build(deps-dev): bump eslint-plugin-mdx from 3.2.0 to 3.3.1
    - closes #135
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- eslint.base.config.mjs
+++ eslint.base.config.mjs
@@ -27,7 +27,17 @@ import pkg from './package.json' with { type: 'json' }
  * @return {Promise<Plugin>}
  *  Eslint plugin
  */
-const plugin = async name => (await import(name)).default
+const plugin = async name => {
+  /**
+   * Plugin module.
+   *
+   * @type {{ default: Plugin } | Plugin}
+   * @const m
+   */
+  const m = await import(name)
+
+  return 'default' in m ? m.default : m
+}
 
 /**
  * Eslint configuration objects.
